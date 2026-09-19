ad26197b3f054264c0e501d6310ae0f1ed153051
Eva-004 <foredajannateva@gmail.com>
2026-09-19 20:50:55 +0600

    create authorities apis

--- index.js
+++ index.js
@@ -21,28 +21,61 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-   const db = client.db("nogorbondhu");
-   const departmentCollection = db.collection('departments');
+    const db = client.db("nogorbondhu");
+    const departmentCollection = db.collection('departments');
+    const authorityCollection = db.collection('authorities');
 
-
-   app.post("/departments",async(req,res)=>{
-    const data = req.body;
-    console.log(data);
-    const departmentData = {
-        departmentName:data.departmentName,
-        description:data.description,
-        status:data.status || "active",
+    app.post("/departments", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const departmentData = {
+        departmentName: data.departmentName,
+        description: data.description,
+        status: data.status || "active",
         createdAt: new Date()
-    }
-    const result = await departmentCollection.insertOne(departmentData);
-    res.json(result);
-   });
+      }
+      const result = await departmentCollection.insertOne(departmentData);
+      res.json(result);
+    });
 
-   app.get("/departments",async(req,res)=>{
-    const result = await departmentCollection.find({}).toArray();
-    res.json(result);
-   });
-   
+    app.get("/departments", async (req, res) => {
+      const result = await departmentCollection.find({}).toArray();
+      res.json(result);
+    });
+
+    app.post("/authorities", async (req, res) => {
+      const authority = req.body;
+
+      const authorityData = {
+        authorityName: authority.authorityName,
+        authorityType: authority.authorityType,
+
+        departmentId: authority.departmentId,
+        departmentName: authority.departmentName,
+
+        coverage: {
+          level: authority.coverage?.level || null,
+          divisionId: authority.coverage?.divisionId || null,
+          districtId: authority.coverage?.districtId || null,
+          upazilaId: authority.coverage?.upazilaId || null,
+        },
+
+        status: authority.status || "active",
+
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      const result = await authorityCollection.insertOne(authorityData);
+
+      res.json(result);
+    });
+
+    app.get("/authorities", async (req, res) => {
+      const result = await authorityCollection.find({}).toArray();
+      res.json(result);
+    });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
